713db4e564c75648481541e571c870e0e888228b
Prabhash Kumar <prabhash.kumar96@gmail.com>
2023-01-04 23:15:57 +0530

    api genrated

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const cors = require('cors')
+
+server.use(cors({
+    origin:"*"
+}))
+
+server.use(middlewares)
+server.use('', router)
+
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
